4877bc03d8ab9d05249845d5ae95ab3b8b6280a6
nguyen-thanhhai <haibkt2@gmail.com>
2018-05-14 16:59:13 +0700

    build

--- src/main/java/PracticeCalendar/Service/ActionService.java
+++ src/main/java/PracticeCalendar/Service/ActionService.java
@@ -1,10 +1,37 @@
 package PracticeCalendar.Service;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class ActionService {
-	public String setAction(String action,HttpServletRequest req) {
-		return null;
-	}
+	public BufferedReader readWeb() throws IOException{
 
+        BufferedReader br = null;
+
+        try {
+
+            URL url = new URL("http://zetcode.com");
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line;
+
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+
+            System.out.println(sb);
+        } finally {
+
+            if (br != null) {
+                br.close();
+            }
+        }
+        return br;
+    }
 }
